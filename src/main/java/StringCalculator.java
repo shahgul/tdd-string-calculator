313444ae27bf3004788dbb8686fe9efb776a990c
shahgul <ansari.shahgul@gmail.com>
2021-02-12 18:17:29 +0530

    refactor variable

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,13 @@
 public class StringCalculator {
     public int Add(String numbers){
-        if (numbers.length()==1)
+        int stringLength = numbers.length();
+        if (stringLength ==1)
             return Integer.parseInt(numbers);
-        else if (numbers.length()==2)
+        else if (stringLength ==2)
             return Integer.parseInt(String.valueOf(numbers.charAt(0))) + Integer.parseInt(String.valueOf(numbers.charAt(1)));
-        else if (numbers.length()>2){
+        else if (stringLength >2){
             int sum = 0;
-            for (int i = 0; i<numbers.length(); i++){
+            for (int i = 0; i< stringLength; i++){
                 char ch = numbers.charAt(i);
                 sum+=ch-48;
             }
